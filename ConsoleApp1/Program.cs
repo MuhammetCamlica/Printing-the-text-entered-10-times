ecc816984068c1b81372984c0fe471f61fbe508e
Muhammet Ali Çamlıca <Mecatr60@gmail.com>
2022-12-30 10:52:50 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+namespace fahrenayt;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("İSMİNİZİ GİRİNİZ..");
+
+        string girilenisim = Console.ReadLine();
+
+        for (int baslangic = 0; baslangic < 10; baslangic++)
+        {
+            Console.WriteLine(girilenisim);
+        }
+        Console.ReadKey();
+
+    }
+}
